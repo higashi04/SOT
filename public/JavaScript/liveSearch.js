6593cb1a7b27df939be04505f4eef930b54ea595
Cesar <cesar.higashi04@gmail.com>
2022-03-19 12:08:52 -0500

    refactored liveSearch, to be included in its own file and reduce amount of written code

--- public/JavaScript/liveSearch.js
+++ public/JavaScript/liveSearch.js
@@ -1,20 +1,38 @@
-const searchBar = document.getElementById('searchBar')
-
-// function search(e) {
-//     fetch('getInv', {
-//         method: 'POST',
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify({payload: e.value})
-//     })
-//   }
-  
-
-//by passing this as a parameter, I'm referring to the element itself
-searchBar.addEventListener('keyup',  function search (e) {
-    fetch('getInv', {
+const searchBar = document.getElementById('searchBar');
+const searchResults = document.getElementById('searchResults');
+const resultsDisplay = document.getElementById('searchResults');
+searchBar.addEventListener('change',  function(e) {
+    e.preventDefault();
+    console.log(searchBar.value)
+    let match = searchBar.value.match(/^[a-zA-Z ]*/);
+    let matchTwo = searchBar.value.match(/\s*/);
+    if (matchTwo[0] === searchBar.value) {
+      searchResults.innerHTML = ''
+      return
+    }
+    if (match[0] === searchBar.value){
+        fetch(route, {
         method: 'POST',
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({payload: e.value})
+        body: JSON.stringify({payload: searchBar.value})
+    }).then(res => res.json()).then(data => {
+      let payload = data.payload
+      searchResults.innerHTML = ''
+      if(payload.length < 1) {
+        searchResults.innerHTML = '<p>No hubo resultados</p>'
+        return
+      }
+      
+      payload.forEach((item, index) => {
+        const newDiv = document.createElement('div')
+        newDiv.className = 'col-6 my-3'
+        const anchor = document.createElement('a')
+        anchor.className = 'btn btn-dark'
+        anchor.href = `${origin}/show/${item._id}`
+        anchor.innerText = item.nombre || item.name
+        newDiv.appendChild(anchor)
+        searchResults.appendChild(newDiv)
+      })
     })
-  })
-
+    }
+})
