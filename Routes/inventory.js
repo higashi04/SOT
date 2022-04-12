f702364222bf6e2a2f27875118eafcf182d4a00b
Cesar <cesar.higashi04@gmail.com>
2022-04-12 15:57:26 -0500

    Se retrabaja ruteado de inventario para proteger rutas de acceso no autorizado

--- Routes/inventory.js
+++ Routes/inventory.js
@@ -4,7 +4,6 @@ const isLoggedIn = require('../middleware/isLoggedin');
 const catchAsync = require('../AsyncErrors');
 const {validaInv} = require('../middleware/validate');
 /////models/////
-const User = require('../models/users');
 const Inv = require('../models/inventory');
 
 
@@ -35,23 +34,29 @@ router.get('/newItem', isLoggedIn, (req, res)=>{
        res.redirect('/inv/show')
    }
 })
+//Ejecutivo de Almacen y Diesel
 router.post('/newItem', isLoggedIn, validaInv,catchAsync(async(req, res)=>{
-    try{
-        const inv = new Inv(req.body)
-        const qty = req.body.cantidad
-        if(qty > 0)
-           {
-            inv.author = req.user 
-            await inv.save();
-            req.flash('success', 'Artículo añadido correctamente')
+    if(req.user.puesto === 'Ejecutivo de Almacen y Diesel') {
+        try{
+            const inv = new Inv(req.body)
+            const qty = req.body.cantidad
+            if(qty > 0)
+               {
+                inv.author = req.user 
+                await inv.save();
+                req.flash('success', 'Artículo añadido correctamente')
+                res.redirect('/inv/newItem')
+         } else {
+             req.flash('error', 'Revise la cantidad')
+             res.redirect('/inv/newItem')
+         }
+        } catch(e){
+            req.flash('error', 'Se produjo un error al intentar el registro.')
             res.redirect('/inv/newItem')
-     } else {
-         req.flash('error', 'Revise la cantidad')
-         res.redirect('/inv/newItem')
-     }
-    } catch(e){
-        req.flash('error', 'Se produjo un error al intentar el registro.')
-        res.redirect('/inv/newItem')
+        }
+    } else {
+        req.flash('error', 'No tiene autorización')
+        res.redirect('/inv/show')
     }
 }));
 router.get('/show/:id', isLoggedIn,catchAsync(async(req, res)=>{
@@ -71,48 +76,60 @@ router.get('/show/:id', isLoggedIn,catchAsync(async(req, res)=>{
     })
 }))
 router.put('/show/:id', isLoggedIn, catchAsync(async(req, res)=>{
-    const {id} = req.params;
-    const qty = parseInt(req.body.cantidad)
-    if(qty>0){
-        const item = await Inv.findById(id)
-        item.cantidad = parseInt(req.body.cantidad) + item.cantidad
-        const updateData = {author: req.user, qty: qty}
-        item.increments.push(updateData)
-        await item.save()
-        res.redirect(`/inv/show/${item._id}`)
+    if(req.user.puesto === 'Ejecutivo de Almacen y Diesel' || req.user.isAdmin) {
+        try {
+            const {id} = req.params;
+            const qty = parseInt(req.body.cantidad)
+            if(qty>0){
+                const item = await Inv.findById(id)
+                item.cantidad = parseInt(req.body.cantidad) + item.cantidad
+                const updateData = {author: req.user, qty: qty}
+                item.increments.push(updateData)
+                await item.save()
+                res.redirect(`/inv/show/${item._id}`)
+            } else {
+                req.flash('error', 'Revise la cantidad.')
+                res.redirect('/inv/show')
+            }
+        } catch(e) {
+            req.flash('error', 'Se produjo un error.')
+            res.redirect('/inv/show')
+        }
     } else {
-        req.flash('error', 'Revise la cantidad')
+        req.flash('error', 'No tiene autorización')
         res.redirect('/inv/show')
     }
 }));
 router.put('/show/:id/remove', isLoggedIn, catchAsync(async(req, res)=>{
-    const {id} = req.params;
-    const qty = parseInt(req.body.cantidad)
-    if(qty>0){
-        const item = await Inv.findById(id)
-        item.cantidad = item.cantidad - parseInt(req.body.cantidad)
-        const updateData = {author: req.user, qty: qty}
-        item.decrements.push(updateData)
-        if(item.cantidad >= 0) {  
-        await item.save()
-        res.redirect(`/inv/show/${item._id}`)
-        } else {
-            req.flash('error', 'Revise la cantidad')
+    if(req.user.puesto === 'Ejecutivo de Almacen y Diesel' || req.user.isAdmin) {
+        try {
+            const {id} = req.params;
+            const qty = parseInt(req.body.cantidad)
+            if(qty>0){
+                const item = await Inv.findById(id)
+                item.cantidad = item.cantidad - parseInt(req.body.cantidad)
+                const updateData = {author: req.user, qty: qty}
+                item.decrements.push(updateData)
+                if(item.cantidad >= 0) {  
+                await item.save()
+                res.redirect(`/inv/show/${item._id}`)
+                } else {
+                    req.flash('error', 'Revise la cantidad')
+                    res.redirect('/inv/show')
+                }
+            } else {
+                req.flash('error', 'Revise la cantidad.')
+                res.redirect('/inv/show')
+            }
+        } catch(e) {
+            req.flash('error', 'Se produjo un error.')
             res.redirect('/inv/show')
         }
     } else {
-        req.flash('error', 'Revise la cantidad')
+        req.flash('error', 'No tiene autorización')
         res.redirect('/inv/show')
     }
 }));
-
-router.post('/show/getMov', isLoggedIn, catchAsync(async(req, res) =>{
-    const gtDate = Date(req.body.payload.searchDateGreaterThan)
-    console.log(gtDate)
-     let search = await Inv.find({"increments": {"fecha": {"$gte": gtDate}}}).exec()
-     console.log('search results: \n' + search)
-     res.send({payload: search})
-}))
 
 router.get('/list', isLoggedIn, catchAsync(async(req, res) => {
     const inv = await Inv.find({}).exec()
