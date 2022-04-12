0b862c7e3f65f13395977e29678848234cd0615c
Cesar <cesar.higashi04@gmail.com>
2022-04-12 09:13:37 -0500

    Se modifica routing de compras para proteger ruta de compra nueva, y para agregar try & catch al momento de renderizar una compra de la DB

--- Routes/compras.js
+++ Routes/compras.js
@@ -35,24 +35,28 @@ router.get('/', isLoggedIn, (req, res) =>{
 });
 router.get('/orden', isLoggedIn, (req, res) => {
     if (req.user.puesto === 'Ejecutivo de Compras' || req.user.isAdmin) {
-    res.render('compras/ordenNew')
+        res.render('compras/ordenNew')
     } else {
         req.flash('error', 'No tiene autorización para esto.')
         res.redirect('/compras/')
     }
-})
-
+});
 router.post('/orden/new', isLoggedIn, catchAsync(async(req, res) => {
-    try{
-        const compra = new Compra(req.body)
-        compra.author = req.user
-        compra.serial = serial()
-        await compra.save()
-        req.flash('success', 'Orden de compra guardada.')
-        res.redirect('/compras/orden')
-    } catch(e) {
-        req.flash('error', 'Se produjo un error.')
-        req.redirect('/compras')
+    if (req.user.puesto === 'Ejecutivo de Compras' || req.user.isAdmin) {
+        try{
+            const compra = new Compra(req.body)
+            compra.author = req.user
+            compra.serial = serial()
+            await compra.save()
+            req.flash('success', 'Orden de compra guardada.')
+            res.redirect('/compras/orden')
+        } catch(e) {
+            req.flash('error', 'Se produjo un error.')
+            res.redirect('/compras')
+        }
+    } else {
+        req.flash('error', 'No tiene autorización para esto.')
+        res.redirect('/compras')
     }
 }));
 
@@ -69,13 +73,18 @@ router.get('/show', isLoggedIn, catchAsync(async(req,res)=>{
 }))
 
 router.get('/show/:id', isLoggedIn, catchAsync(async(req, res) =>{
-    await Compra.findById(req.params.id).exec(function(err, foundOrder) {
-        if(err){
-            req.flash('error', 'Se produjo un error')
-            return res.redirect('/');
-        }
-        res.render('compras/orderDetails', {order: foundOrder});
-    })
+    try {
+        await Compra.findById(req.params.id).exec(function(err, foundOrder) {
+            if(err){
+                req.flash('error', 'Se produjo un error')
+                return res.redirect('/');
+            }
+            res.render('compras/orderDetails', {order: foundOrder});
+        })
+    } catch(e) {
+        req.flash('error', 'Se produjo un error.')
+        res.redirect('/compras/show')
+    }
 }))
 
 module.exports = router;
