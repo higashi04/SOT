9711644754a1474dd13cae8ba4aa3a3cc0764a55
Cesar <cesar.higashi04@gmail.com>
2022-04-27 15:51:07 -0500

    bug fix

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60*24,
         maxAge: 1000 * 60 * 60*24
     }
