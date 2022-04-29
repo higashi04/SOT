e536b72b16b47ee114b59010a65a83587a3fa9a5
Cesar <cesar.higashi04@gmail.com>
2022-04-29 14:55:21 -0500

    Se retrabaja seccion de asistencia de choferes, pendiente retrabajar parte de registrar asistencia diaria

--- Routes/driversAttendance.js
+++ Routes/driversAttendance.js
@@ -4,100 +4,7 @@ const isLoggedIn = require('../middleware/isLoggedin');
 const catchAsync = require('../AsyncErrors');
 //models//
 const driver = require('../models/drivers');
-const driverAttendance = require('../models/driversAttendance');
 const driverWeek = require('../models/driverWeekly');
-const drivers = require('../models/drivers');
-
-const day = async(day, newAttendance, list, choferes) =>{
-    switch (day.day) {
-        case 'Lunes':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.monday = newAttendance
-            await list.save()
-            break;
-        case 'Martes':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.tuesday = newAttendance
-            await list.save()
-            break;
-        case 'Miércoles':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.wednesday = newAttendance
-            await list.save()
-            break;    
-        case 'Jueves':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.thursday = newAttendance
-            await list.save()
-            break;    
-        case 'Viernes':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.friday = newAttendance
-            await list.save()
-            break;    
-        case 'Sábado':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.saturday = newAttendance
-            await list.save()
-            break;    
-        case 'Domingo':
-            choferes.forEach((item, index) =>{
-                newAttendance.driver.push(item._id)
-                newAttendance.inOne.push(day.inOne[index] === 'true' ? true : false)
-                newAttendance.outOne.push(day.outOne[index] === 'true' ? true : false)
-                newAttendance.inTwo.push(day.inTwo[index] === 'true' ? true : false)
-                newAttendance.outTwo.push(day.outTwo[index] === 'true' ? true : false)
-            })
-            await newAttendance.save()
-            list.sunday = newAttendance
-            await list.save()
-            break;    
-        default:
-            break;
-    }
-}
 
 router.get('/', isLoggedIn, catchAsync(async(req, res) => {
     const lists = await driverWeek.find()
@@ -108,6 +15,7 @@ router.post('/', isLoggedIn, catchAsync(async(req,res) => {
         try {
             const newAttendance = new driverWeek(req.body)
             await newAttendance.save()
+            req.flash('success', 'Se crea el registro.')
             res.redirect('/driverAttendance')
         } catch(e) {
             req.flash('error', 'Se produjo un error.')
@@ -122,24 +30,20 @@ router.post('/', isLoggedIn, catchAsync(async(req,res) => {
 }));
 router.get('/:id', isLoggedIn, catchAsync(async(req, res)=>{
     const list = await driverWeek.findById(req.params.id)
-    .populate({path: 'monday', 
-    populate: {
-        path: 'driver', 
-        populate: {
-            path: 'bus'}}}).populate({path: 'tuesday'})
-    .populate({path: 'wednesday'}).populate({path: 'thursday'})
-    .populate({path: 'friday'}).populate({path: 'saturday'}).populate({path: 'sunday'})
     const drivers = await driver.find({}).populate({path: 'bus'}).exec()
     res.render('driversAttendance/list', {list, drivers})
 }))
 router.put('/:id', isLoggedIn, catchAsync(async(req,res) =>{
     try {
-        const {id} = req.params
-        const newAttendance = new driverAttendance(day.body)
-        const choferes = await drivers.find({})
-        const list = await driverWeek.findById(id)
-        day(req.body, newAttendance, list, choferes)
-        res.redirect('/driverAttendance')
+        const list = await driverWeek.findById(req.params.id)
+        const {asistencias, faltas, retardos, justificado} = req.body
+        asistencias.forEach(i => list.asistencias.push(i))
+        faltas.forEach(i => list.faltas.push(i))
+        retardos.forEach(i => list.retardos.push(i))
+        justificado.forEach(i => list.justificado.push(i))
+        await list.save()
+        req.flash('success', 'Se guarda el registro.')
+        res.redirect(`/driverAttendance/${list._id}`)
     } catch(e) {
         console.log(e)
         res.redirect('/driverAttendance')
--- models/driverWeekly.js
+++ models/driverWeekly.js
@@ -2,43 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const driverWeekSchema = new Schema({
-    kind: {
-        type: String,
-        enum: ['Horario Normal', 'Tiempo Extra']
-    },
+    asistencias: [Number],
+    faltas: [Number],
+    retardos: [Number],
+    justificado: [Number],
+    choferes: [String],
+    unidad: [String],
+    tipoDeUnidad: [String],
+    mes: String,
     date: {
         type: Date,
-        default: () => Date.now(),
-        immutable: true
-    },
-    monday: {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    tuesday:  {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    wednesday: {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    thursday:  {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    friday:  {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    saturday:  {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
-    sunday: {
-        type: Schema.Types.ObjectId,
-        ref: 'driverAttendance'
-    },
+        default: () => Date.now()
+    }
+
 })
 
 module.exports = mongoose.model('week', driverWeekSchema)
